0150a5d3f17e2d018e964922f9e1d9e5790d0c4f
Nate Wolfe <nwolfe@puppetlabs.com>
2013-07-08 23:32:17 -0700

    Hacky version of fancy UI with minecraft screenshot

--- Main.java
+++ Main.java
@@ -1,11 +1,36 @@
-import javax.swing.JOptionPane;
+import javax.swing.*;
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.lang.String;
 import java.util.Properties;
 import java.io.*;
 
 public class Main {
-
   public static void main(String[] args) throws Exception {
+    fancyMOTD();
+  }
+
+  private static void fancyMOTD() throws Exception {
+    String filename = "./resources/multiplayer_screen.png";
+    final Image background = ImageIO.read(new File(filename));
+
+    JPanel p = new JPanel() {
+      public void paint(Graphics g) {
+        super.paint(g);
+        g.drawImage(background, -57, -57, this);
+      }
+    };
+
+    JFrame f = new JFrame();
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    f.setTitle("Minecraft Server MOTD");
+    f.add(p);
+    f.pack();
+    f.setSize(854, 502);
+    f.show();
+  }
+
+  private static void basicMOTD() throws Exception {
     File server = new File("/Users/nwolfe/Downloads/server.properties");
     Properties config = new Properties();
     config.load(new FileInputStream(server));
@@ -17,8 +42,9 @@ public class Main {
     config.store(new FileOutputStream(server), "");
   }
 
-  public static String modifyMOTD(String motd) {
+  private static String modifyMOTD(String motd) {
     String modifiedMOTD = JOptionPane.showInputDialog("Enter the MOTD", motd);
+    // TODO: Inject appropriate spacing into motd
     return modifiedMOTD;
   }
 }
